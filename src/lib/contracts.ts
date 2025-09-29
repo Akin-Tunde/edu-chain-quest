3229bf9b28537e8f9f03369b87fc2dbb1820bd9a
Akin-Tunde <wang23hao@gmail.com>
2025-09-29 07:44:57 +0000

    done

---
+++ src/lib/contracts.ts
@@ -0,0 +1,10 @@
+// src/lib/contracts.ts
+//proxy address
+export const EDUTokenAddress = "0xd3240460DECdd7cC6Cd465d58cAFDE66fbcC8551";
+export const CourseRegistryAddress = "0x66af7F20AA0Ed1697Aade356f568E28243bF684a";
+export const AchievementBadgeAddress = "0xCAa3cEB38076E600EA0A07f2D2Acab9348E56D43";
+export const RewardDistributionAddress = "0x1cbcA43488E4c467FfE64D412DC6df6b946aB71b";
+export const EDUStakingAddress = "0xa91C2e49AD3143ea7827f7bf76a64142166140f8";
+// Add CoursePaymentAddress here when you deploy it.
+
+// You will add the ABIs below these addresses
